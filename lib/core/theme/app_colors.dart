55f334a32b6e85c0b418e736cfbec63d824b0abf
HX <hx.net1@gmail.com>
2026-07-19 12:43:35 +0330

    add all colors in app

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SolidColors {
+  SolidColors._();
+
+  static const Color primaryTextColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color secondaryTextColor = Color.fromARGB(255, 123, 123, 139);
+  static const Color navigationsBar = Color.fromARGB(255, 24, 26, 38);
+  static const Color scondaryColor = Color.fromARGB(255, 251, 101, 128);
+  static const Color inputBackgroundColor = Color.fromARGB(255, 29, 25, 44);
+  static const Color inputTextColor = Color.fromARGB(255, 116, 119, 160);
+  static const Color musicCardColor = Color.fromARGB(255, 11, 11, 21);
+  static const Color mutedIconColor = Color.fromARGB(255, 92, 94, 111);
+  static const Color facebockButtonColor = Color.fromARGB(255, 57, 87, 154);
+}
+class GradiantColors {
+  GradiantColors._();
+
+  static const List<Color> bgColor = [Color.fromARGB(255, 45, 44, 60),Color.fromARGB(255, 25, 25, 37)];
+  static const List<Color> buttonColor = [Color.fromARGB(255, 241, 23, 117),Color.fromARGB(255, 251, 101, 128)];
+
+}
