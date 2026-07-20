9b2a30988b6065a05bfdc352b418f8c33a4d3d0e
HX <hx.net1@gmail.com>
2026-07-20 18:46:50 +0330

    Update Font and Color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -12,12 +12,18 @@ class AuthColors {
   static const Color mutedIconColor = Color.fromARGB(255, 92, 94, 111);
   static const Color facebockButtonColor = Color.fromARGB(255, 57, 87, 154);
 }
+
 class AuthGradiant {
   AuthGradiant._();
 
-  static const List<Color> bgColor = [Color.fromARGB(255, 45, 44, 60),Color.fromARGB(255, 25, 25, 37)];
-  static const List<Color> buttonColor = [Color.fromARGB(255, 241, 23, 117),Color.fromARGB(255, 251, 101, 128)];
-
+  static const List<Color> bgColor = [
+    Color.fromARGB(255, 45, 44, 60),
+    Color.fromARGB(255, 25, 25, 37),
+  ];
+  static const List<Color> buttonColor = [
+    Color.fromARGB(255, 241, 23, 117),
+    Color.fromARGB(255, 251, 101, 128),
+  ];
 }
 
 class AppColors {
@@ -26,12 +32,24 @@ class AppColors {
   static const Color secondaryTextColor = Color.fromARGB(255, 153, 153, 159);
   static const Color navigationsBar = Color.fromARGB(255, 48, 48, 51);
   static const Color selectedTextColor = Color.fromARGB(255, 45, 206, 239);
-  static const Color bgColor = Color.fromARGB(255, 22, 26, 26);
+  static const Color bgColor = Color.fromARGB(255, 20, 20, 20);
 }
 
 class AppGradiant {
   AppGradiant._();
-  static const List<Color> bgGradiant = [Color.fromARGB(255, 127, 70, 219),Color.fromARGB(255, 44, 47, 109),Color.fromARGB(255, 22, 26, 26)];
-  static const List<Color> selectedIconColor = [Color.fromARGB(255, 155, 45, 239),Color.fromARGB(255, 45, 155, 239),Color.fromARGB(255, 45, 206, 239)];
-  static const List<Color> buttonColor = [Color.fromARGB(255, 155, 45, 239),Color.fromARGB(255, 45, 155, 239),Color.fromARGB(255, 53, 237, 251)];
+  static const List<Color> bgGradiant = [
+    Color.fromARGB(255, 0, 53, 102),
+    Color.fromARGB(255, 0, 29, 61),
+    Color.fromARGB(255, 20, 20, 20),
+  ];
+  static const List<Color> selectedIconColor = [
+    Color.fromARGB(255, 155, 45, 239),
+    Color.fromARGB(255, 45, 155, 239),
+    Color.fromARGB(255, 45, 206, 239),
+  ];
+  static const List<Color> buttonColor = [
+    Color.fromARGB(255, 155, 45, 239),
+    Color.fromARGB(255, 45, 155, 239),
+    Color.fromARGB(255, 53, 237, 251),
+  ];
 }
--- lib/core/theme/app_text_style.dart
+++ lib/core/theme/app_text_style.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
+class AppFonts {
+  AppFonts._();
+
+  static const String primary = 'CircularStd';
+  static const String secondary = 'Poppins';
+  static const String thirty = 'lato';
+}
+
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
-      fontFamily: 'CircularStd',
-
+      fontFamily: AppFonts.primary,
       textTheme: const TextTheme(
         titleLarge: TextStyle(
           fontSize: 33,
@@ -55,4 +62,60 @@ class AppTheme {
       ),
     );
   }
+}
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const hero = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 50,
+    fontWeight: FontWeight.w800,
+    color: AppColors.primaryTextColor,
+  );
+
+
+  static const appBar = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryTextColor,
+  );
+
+  static const sectionTitle = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryTextColor,
+  );
+  static const itemTitle = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryTextColor,
+  );
+  static const likeCount = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryTextColor,
+  );
+  static const description = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryTextColor,
+  );
+  static const moreTitle = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryTextColor,
+  );
+  static const buttonLabel = TextStyle(
+    fontFamily: AppFonts.thirty,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryTextColor,
+  );
 }
